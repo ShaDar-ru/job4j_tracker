1433b1e6206f18486aa35246af25809dd8c0021a
ShaDar <alexterentev90@gmail.com>
2021-01-20 13:28:07 +0300

    7. Date. Отображение даты. [#429420]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,10 +7,6 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
     public Item() {
     }
 
@@ -25,6 +21,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
