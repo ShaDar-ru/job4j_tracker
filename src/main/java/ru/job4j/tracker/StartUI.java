a69c6495b2e294ff59d655e069ff05398fd95a50
ShaDar <alexterentev90@gmail.com>
2021-01-21 14:47:12 +0300

    5. Tracker - хранилище [#429433]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,7 +7,14 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String timecreate =  item.getCreated().format(formatter);
+        String timecreate = item.getCreated().format(formatter);
         System.out.println(timecreate);
+
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+
+        System.out.println(tracker.findById(item.getId()));
+        //Для проверки:
+        System.out.println(tracker.findById(item.getId()).getId());
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,32 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item temp = items[i];
+            if (temp != null) {
+                itemsWithoutNull[size] = temp;
+                size++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        return itemsWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsWithName = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].getName().equals(key)) {
+                itemsWithName[size] = items[i];
+                size++;
+            }
+        }
+        itemsWithName = Arrays.copyOf(itemsWithName, size);
+        return itemsWithName;
     }
 }
