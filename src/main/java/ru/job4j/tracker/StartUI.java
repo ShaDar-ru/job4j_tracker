a529d8af115c869dc1c03fce9a07fcb5e1f40aaf
ShaDar <alexterentev90@gmail.com>
2021-01-26 09:36:07 +0300

    4.2. Статические методы. [#429360]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,62 +9,22 @@ public class StartUI {
             int select = Integer.valueOf(str);
             switch (select) {
                 case 0:
-                    System.out.println("=== Create a new Item ====");
-                    String nameNewItem = input.askStr("Введите имя новой заявки: ");
-                    Item newItem = new Item(nameNewItem);
-                    tracker.add(newItem);
+                    StartUI.createItem(input, tracker);
                     break;
                 case 1:
-                    System.out.println("=== Show all items ====");
-                    Item[] allItems = tracker.findAll();
-                    for (int i = 0; i < allItems.length; i++) {
-                        System.out.println(allItems[i].toString());
-                    }
+                    StartUI.showAllItems(tracker);
                     break;
                 case 2:
-                    System.out.println("=== Edit Item ====");
-                    String changedId = input.askStr("Введите номер изменяемой заявки: ");
-                    int replaceId = Integer.valueOf(changedId);
-                    String nameRepItem = input.askStr("Введите имя новой заявки: ");
-                    Item replacedItem = new Item(nameRepItem);
-                    if (tracker.replace(replaceId, replacedItem)) {
-                        System.out.println("Replace success");
-                    } else {
-                        System.out.println("Replace error");
-                    }
+                    StartUI.replaceItem(input, tracker);
                     break;
                 case 3:
-                    System.out.println("=== Delete Item ====");
-                    String searchIdStrDel = input.askStr("Введите номер удаляемой заявки: ");
-                    int deleteId = Integer.valueOf(searchIdStrDel);
-                    if (tracker.delete(deleteId)) {
-                        System.out.println("Delete success");
-                    } else {
-                        System.out.println("Delete error");
-                    }
+                    StartUI.deleteItem(input, tracker);
                     break;
                 case 4:
-                    System.out.println("=== Find Item by Id ====");
-                    String searchIdStr = input.askStr("Введите номер искомой заявки: ");
-                    int searchId = Integer.valueOf(searchIdStr);
-                    Item searchedItemById = tracker.findById(searchId);
-                    if (searchedItemById != null) {
-                        System.out.println(searchedItemById.toString());
-                    } else {
-                        System.out.println("Заявка с таким id не найдена");
-                    }
+                    StartUI.findItemById(input, tracker);
                     break;
                 case 5:
-                    System.out.println("=== Find Item by Name ====");
-                    String searchName = input.askStr("Введите имя искомой заявки: ");
-                    Item[] searchedItemsByName = tracker.findByName(searchName);
-                    if (searchedItemsByName.length>0) {
-                        for (Item it : searchedItemsByName) {
-                            System.out.println(it.toString());
-                        }
-                    } else {
-                        System.out.println("Заявки с таким именем не найдены");
-                    }
+                    StartUI.findItemByName(input, tracker);
                     break;
                 case 6:
                     run = false;
@@ -72,6 +32,70 @@ public class StartUI {
                 default:
                     break;
             }
+        }
+    }
+
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String nameNewItem = input.askStr("Введите имя новой заявки: ");
+        Item newItem = new Item(nameNewItem);
+        tracker.add(newItem);
+    }
+
+    public static void showAllItems(Tracker tracker) {
+        System.out.println("=== Show all items ====");
+        Item[] allItems = tracker.findAll();
+        for (int i = 0; i < allItems.length; i++) {
+            System.out.println(allItems[i].toString());
+        }
+    }
+
+    public static void replaceItem(Input input, Tracker tracker) {
+        System.out.println("=== Edit Item ====");
+        String changedId = input.askStr("Введите номер изменяемой заявки: ");
+        int replaceId = Integer.valueOf(changedId);
+        String nameRepItem = input.askStr("Введите имя новой заявки: ");
+        Item replacedItem = new Item(nameRepItem);
+        if (tracker.replace(replaceId, replacedItem)) {
+            System.out.println("Replace success");
+        } else {
+            System.out.println("Replace error");
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        System.out.println("=== Delete Item ====");
+        String searchIdStrDel = input.askStr("Введите номер удаляемой заявки: ");
+        int deleteId = Integer.valueOf(searchIdStrDel);
+        if (tracker.delete(deleteId)) {
+            System.out.println("Delete success");
+        } else {
+            System.out.println("Delete error");
+        }
+    }
+
+    public static void findItemById(Input input, Tracker tracker) {
+        System.out.println("=== Find Item by Id ====");
+        String searchIdStr = input.askStr("Введите номер искомой заявки: ");
+        int searchId = Integer.valueOf(searchIdStr);
+        Item searchedItemById = tracker.findById(searchId);
+        if (searchedItemById != null) {
+            System.out.println(searchedItemById.toString());
+        } else {
+            System.out.println("Заявка с таким id не найдена");
+        }
+    }
+
+    public static void findItemByName(Input input, Tracker tracker) {
+        System.out.println("=== Find Item by Name ====");
+        String searchName = input.askStr("Введите имя искомой заявки: ");
+        Item[] searchedItemsByName = tracker.findByName(searchName);
+        if (searchedItemsByName.length > 0) {
+            for (Item it : searchedItemsByName) {
+                System.out.println(it.toString());
+            }
+        } else {
+            System.out.println("Заявки с таким именем не найдены");
         }
     }
 
