0826454418407e8f090645017574b004e12a9807
ShaDar <alexterentev90@gmail.com>
2021-01-26 12:07:29 +0300

    5. Input и полиморфизм. [#429357]

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,90 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StartUITest {
+
+    @Test
+    public void init() {
+    }
+
+    @Test
+    public void whenCreateItem() {
+        String[] answers = {"Fix PC", "bug1", "bug2"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        for (int i = 0; i < answers.length; i++) {
+            StartUI.createItem(input, tracker);
+            Item created = tracker.findAll()[i];
+            Item expected = new Item(answers[i]);
+            assertThat(created.getName(), is(expected.getName()));
+        }
+    }
+
+    @Test
+    public void showAllItems() {
+        String[] answers = {"bug", "fix", "bug2", "fix"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        Item[] expected = new Item[answers.length];
+        for (int i = 0; i < answers.length; i++) {
+            StartUI.createItem(input, tracker);
+            Item exp = new Item(i + 1, answers[i]);
+            expected[i] = exp;
+        }
+        Item[] created = tracker.findAll();
+        for (int i = 0; i < answers.length; i++) {
+            assertThat(created[i].toString(), is(expected[i].toString()));
+        }
+    }
+
+    @Test
+    public void replaceItem() {
+        String[] questions = {"bug", "bug1", "bug2"};
+        Input input = new StubInput(questions);
+        Tracker tracker = new Tracker();
+        for (int i = 0; i < questions.length; i++) {
+            StartUI.createItem(input, tracker);
+        }
+        Item[] expected = tracker.findAll();
+        expected[1] = new Item(2, "fix");
+        String[] answrs = {"2", "fix"};
+        Input answ = new StubInput(answrs);
+        StartUI.replaceItem(answ, tracker);
+        Item[] created = tracker.findAll();
+        for (int i = 0; i < questions.length; i++) {
+            assertThat(created[i].getName(), is(expected[i].getName()));
+        }
+    }
+
+    @Test
+    public void deleteItem2() {
+        String[] answers = {"bug", "bug2", "bug3"};
+        Input inp = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        for (int i = 0; i < answers.length; i++) {
+            StartUI.createItem(inp, tracker);
+        }
+        Input delInp = new StubInput(new String[]{"2"});
+        StartUI.deleteItem(delInp, tracker);
+        Item[] rsl = tracker.findAll();
+        for (int i = 0; i < tracker.findAll().length; i++) {
+            Item exp = new Item(i + 1, answers[i]);
+            if (exp.getId() != 2) {
+                assertThat(rsl[i].getName(), is(exp.getName()));
+            }
+        }
+    }
+
+    @Test
+    public void findItemById() {
+    }
+
+    @Test
+    public void findItemByName() {
+    }
+}
