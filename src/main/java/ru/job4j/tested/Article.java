0ea493f6edc2fe47ca7879c3f313151c5f0ee256
ShaDar <alexterentev90@gmail.com>
2021-04-04 15:58:36 +0300

    UPD: bugfix

--- src/main/java/ru/job4j/tested/Article.java
+++ src/main/java/ru/job4j/tested/Article.java
@@ -5,13 +5,12 @@ import java.util.Map;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        String originRep = origin.replaceAll("[\\p{Punct}]", "");
+        String originRep = origin.toLowerCase().replaceAll("[\\p{Punct}]", "");
         String[] originArray = originRep.split("[\\p{Blank}]");
-        String[] lineArray = line.split("[\\p{Blank}]");
+        String[] lineArray = line.toLowerCase().split("[\\p{Blank}]");
         Map<Integer, String> originMap = new HashMap<>();
         for (int i = 0; i < originArray.length; i++) {
             originMap.put(i, originArray[i]);
-            System.out.println(originArray[i]);
         }
         for (String s : lineArray) {
             if (!originMap.containsValue(s)) {
