a28ff20ae5d17ef1eda7b3b00d44a5edce692dbd
ShaDar <alexterentev90@gmail.com>
2021-04-04 15:43:48 +0300

    1. Генератор текста

---
+++ src/main/java/ru/job4j/tested/Article.java
@@ -0,0 +1,23 @@
+package ru.job4j.tested;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+        String originRep = origin.replaceAll("[\\p{Punct}]", "");
+        String[] originArray = originRep.split("[\\p{Blank}]");
+        String[] lineArray = line.split("[\\p{Blank}]");
+        Map<Integer, String> originMap = new HashMap<>();
+        for (int i = 0; i < originArray.length; i++) {
+            originMap.put(i, originArray[i]);
+            System.out.println(originArray[i]);
+        }
+        for (String s : lineArray) {
+            if (!originMap.containsValue(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
