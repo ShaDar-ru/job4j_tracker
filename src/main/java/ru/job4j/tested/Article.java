946f32bbbd4e51e7f934ac991c68be57e197dd35
ShaDar <alexterentev90@gmail.com>
2021-04-05 08:54:12 +0300

    1. Экзамен. Коллекции. [#429324]

--- src/main/java/ru/job4j/tested/Article.java
+++ src/main/java/ru/job4j/tested/Article.java
@@ -1,19 +1,15 @@
 package ru.job4j.tested;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
         String originRep = origin.toLowerCase().replaceAll("[\\p{Punct}]", "");
         String[] originArray = originRep.split("[\\p{Blank}]");
+        Set<String> originSet = new HashSet<>(Arrays.asList(originArray));
         String[] lineArray = line.toLowerCase().split("[\\p{Blank}]");
-        Map<Integer, String> originMap = new HashMap<>();
-        for (int i = 0; i < originArray.length; i++) {
-            originMap.put(i, originArray[i]);
-        }
         for (String s : lineArray) {
-            if (!originMap.containsValue(s)) {
+            if (!originSet.contains(s)) {
                 return false;
             }
         }
