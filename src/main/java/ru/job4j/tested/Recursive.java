9b7ff0cab8b7b08399b12470261e50a6c2768d6e
ShaDar <alexterentev90@gmail.com>
2021-06-20 16:44:12 +0300

    рекурсия bugfix

--- src/main/java/ru/job4j/tested/Recursive.java
+++ src/main/java/ru/job4j/tested/Recursive.java
@@ -6,12 +6,16 @@ public class Recursive {
     public static void recurse(int a, int[] nums) {
         int cnt = (nums.length - 1) / 2;
         System.out.print(nums[cnt] + " ");
-        if (nums.length > 1) {
+        //if (nums.length > 1) {
             int[] n1 = Arrays.copyOfRange(nums, cnt + 1, nums.length);
-            recurse(a, n1);
+            if (n1.length > 0) {
+                recurse(a, n1);
+            }
             int[] n2 = Arrays.copyOfRange(nums, 0, cnt);
-            recurse(a, n2);
-        }
+            if (n2.length > 0) {
+                recurse(a, n2);
+            }
+        //}
     }
 
     public static void main(String[] args) {
